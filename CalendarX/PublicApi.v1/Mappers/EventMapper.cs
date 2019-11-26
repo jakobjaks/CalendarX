de477ecad0440ed64193cbed0357f127c6ea6d4b
jaroot <jaroot@ttu.ee>
2019-11-26 14:27:36 +0200

    Complete rework of the data flow, added a business logic layer, Commented out controllers that arent in use until the backend is complete Will add them back one by one or maybe remove some Added mappers for 3 types of domains, events administrativeunits and locations
    Will implement more in next commits

---
+++ CalendarX/PublicApi.v1/Mappers/EventMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using externalDTO = PublicApi.v1.DTO;
+using internalDTO = BLL.App.DTO;
+
+namespace PublicApi.v1.Mappers
+{
+    public class EventMapper
+    {
+        public TOutObject Map<TOutObject>(object inObject)
+            where TOutObject : class
+        {
+            if (typeof(TOutObject) == typeof(externalDTO.Event))
+            {
+                // map internal to external
+                return MapFromBLL((internalDTO.Event) inObject) as TOutObject;
+            }
+
+            if (typeof(TOutObject) == typeof(internalDTO.Event))
+            {
+                // map from external to internal
+                return MapFromExternal((externalDTO.Event) inObject) as TOutObject;
+            }
+            throw new InvalidCastException($"No conversion from {inObject.GetType().FullName} to {typeof(TOutObject).FullName}");
+        }
+
+        public static externalDTO.Event MapFromBLL(internalDTO.Event Event)
+        {
+            var res = Event == null ? null : new externalDTO.Event
+            {
+                Id = Event.Id,
+                Name = Event.Name,
+                Description = Event.Description,   
+                AppUserId = Event.AppUserId,
+            };
+
+            return res;
+        }
+
+        public static internalDTO.Event MapFromExternal(externalDTO.Event Event)
+        {
+            Console.WriteLine(Event.Name + "BLL");
+
+            var res = Event == null ? null : new internalDTO.Event
+            {
+                Id = Event.Id,
+                Name = Event.Name,
+                Description = Event.Description,   
+                AppUserId = Event.AppUserId,
+            };
+            Console.WriteLine(res.Name + "BLL");
+
+            return res;
+        }
+    }
+}
